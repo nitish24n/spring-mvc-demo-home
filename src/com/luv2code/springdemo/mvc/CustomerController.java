1cde378a92f811f15751b0166117c414a7fe12b3
nitish24n <nitish24m@gmail.com>
2018-05-17 23:22:57 +0530

    Add pre-processing code with InitBinder

--- src/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -2,15 +2,31 @@ package com.luv2code.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// Add an InitBinder to convert trim input strings
+	// Remove leading and trailing white spaces
+	// Resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor ste = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class,ste);
+		
+	}
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model model) {
