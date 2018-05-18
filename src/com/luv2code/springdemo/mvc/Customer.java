1cd669a1b3a68c3035a533f63872012ee022cbff
nitish24n <nitish24m@gmail.com>
2018-05-18 11:10:07 +0530

    How to make integer field required

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -14,9 +14,10 @@ public class Customer {
 	@Size(min=1,message="is Required")
 	private String lastName;
 	
+	@NotNull(message="is Required")
 	@Min(value=0,message="must be greater or equal to zero")
 	@Max(value=10,message="must be less or equal to ten")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}",message="only 5 char/digits")
 	private String postalCode;
@@ -33,10 +34,10 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	public String getPostalCode() {
